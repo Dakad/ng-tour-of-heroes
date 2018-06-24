d366663c266854b5e9628b3cf2b89bba5bd617f3
Dakad <3106338+Dakad@users.noreply.github.com>
2018-06-24 20:24:13 +0200

    Ajust HeroService to use HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,30 +1,56 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
-import { HEROES } from './heroes.mock';
 import { Hero } from './hero';
 import { MsgService } from './msg.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+const { API_URL } = environment; // URL to web api
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private msgService: MsgService) {}
+  constructor(private http: HttpClient, private msgService: MsgService) {}
 
-  /*
-  getHeroes(): Hero[] {
-    return HEROES;
+  /** Log a HeroService msg with the msgService */
+  private log(msg: string) {
+    this.msgService.add('[HeroService] ' + msg);
   }
- */
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.msgService.add('[HeroService] Fetching heroes ...');
-    return of(HEROES);
+    this.log('Fetching heroes ...');
+    return this.http.get<Hero[]>(API_URL);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.msgService.add('[HeroService] Fetching Hero #' + id);
-    return of(HEROES.find(hero => hero.id == id));
+    this.log('Fetching Hero #' + id);
+    const url = `${API_URL}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`Fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>('getHero id=' + id))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -2,3 +2,14 @@ export class Hero {
   id: number;
   name: string;
 }
+
+export const HEROES: Hero[] = [
+  { id: 1009187 | 1011130, name: 'Black Panther' },
+  { id: 1009220 | 1010913, name: 'Captain America' },
+  { id: 1009351, name: 'HULK' },
+  { id: 1009664, name: 'Thor' },
+  { id: 1010740, name: 'Winter Soldier' },
+  { id: 1009718, name: 'Wolverine' },
+  { id: 1009262, name: 'Daredevil' },
+  { id: 1009282, name: 'Doctor Strange' }
+];
