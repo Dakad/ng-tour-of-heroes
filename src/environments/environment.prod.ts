d931ea9447c0e80d9e96dcca3c4e18ab1aea3bf1
Dakad <3106338+Dakad@users.noreply.github.com>
2018-06-24 19:59:37 +0200

    Add api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'https://gateway.marvel.com:443/v1/public/',
-  API_KEY: ''
+  API_KEY: 'ea3505e98cb23e22ed540c7a2f356e8c'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   API_URL: 'https://gateway.marvel.com:443/v1/public/',
-  API_KEY: ''
+  API_KEY: 'ea3505e98cb23e22ed540c7a2f356e8c'
 };
 
 /*
