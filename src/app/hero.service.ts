363fa8bc53dec731f94400cf1c8f727edc0fa883
Dakad <3106338+Dakad@users.noreply.github.com>
2018-06-24 19:28:09 +0200

    Add API vars into HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment as ENV } from '../environments/environment';
 
 import { HEROES } from './heroes.mock';
 import { Hero } from './hero';
 import { MsgService } from './msg.service';
 
+const { API_URL, API_KEY } = ENV; // API_URL and API_KEY to MARVEL API
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private msgService: MsgService) {}
+  constructor(private http: HttpClient, private msgService: MsgService) {}
 
-  /*
-  getHeroes(): Hero[] {
-    return HEROES;
+  /** Log a HeroService message with the MessageService */
+  private log(msg: string) {
+    this.msgService.add('[HeroService] ' + msg);
   }
- */
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
